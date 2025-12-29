09f5578f23db20e8c509644cb0a5fecdaf2f02bb
ashis <161737873+axhis8@users.noreply.github.com>
2025-12-30 00:08:06 +0100

    Code Functions of Delete, Clear, Digits & Operators

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const multiply = function(a, b) {
 }
 
 const divide = function(a, b) {
-   return a / b; 
+   return a == 0 || b == 0 ? "ERROR" : a / b; 
 }
 
 let firstNum;
@@ -21,20 +21,53 @@ let operator;
 function operate(operator, a, b) {
     switch (operator) {
         
-        case "+": {
-            add(a, b);
-        } 
+        case "+": 
+            return add(a, b);
 
-        case "-": {
-            subtract(a, b);
-        } 
+        case "-":
+            return subtract(a, b);
 
-        case "*": {
-            multiply(a, b);
-        } 
+        case "×":
+            return multiply(a, b);
 
-        case "/": {
-            divide(a, b);
-        } 
+        case "+":
+            return divide(a, b)
     }
 }
+
+const display = document.querySelector(".display");
+const buttons = document.querySelector(".buttons");
+const digits = document.querySelectorAll(".digit");
+
+digits.forEach(digit => digit.addEventListener('click', () => display.textContent += digit.textContent)); 
+// Adds Event Listener for every Digit (0-9)
+
+const delBtn = document.querySelector(".delete");
+const clearBtn = document.querySelector(".clear");
+
+delBtn.addEventListener('click', () => display.textContent = display.textContent.slice(0, -1));
+
+clearBtn.addEventListener('click', () => {
+    display.textContent = ""
+    firstNum = 0;
+    secondNum = 0;
+    operator = "";
+});
+
+const operatorBtns = document.querySelectorAll(".operator");
+const operationBtn = document.querySelector(".operation");
+
+
+operatorBtns.forEach((btn) => btn.addEventListener('click', () => {
+        firstNum = display.textContent;
+        display.textContent = "";
+        operator = btn.textContent;
+    }));
+// Adds Event Listener for every Operator (+, -, *, /)
+
+
+operationBtn.addEventListener('click', () => {
+    secondNum = display.textContent;
+    display.textContent = operate(operator, Number(firstNum), Number(secondNum));
+})
+
