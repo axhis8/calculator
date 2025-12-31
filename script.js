44ce770e5c368ad56dfec2be42b646ec356f3e51
ashis <161737873+axhis8@users.noreply.github.com>
2025-12-31 06:48:44 +0100

    Fix script bug
    Fixed the bug where if you got the result and typed a number, the new digit would add to the result- thus changing it to now removing the previous result, after typing in a new number.

--- script.js
+++ script.js
@@ -11,65 +11,79 @@ const multiply = function(a, b) {
 }
 
 const divide = function(a, b) {
-   return a == 0 ? "ERROR" : a / b; 
-}
-
-let firstNum;
-let secondNum;
-let operator;
+   return b == 0 ? "ERROR" : a / b;
+}   
 
 function operate(operator, a, b) {
+
     switch (operator) {
         
         case "+": 
             return add(a, b);
 
         case "-":
-            return subtract(a, b);
+            return subtract(a, b)
 
         case "×":
             return multiply(a, b);
 
-        case "+":
-            return divide(a, b)
+        case "÷":
+            return divide(a, b);
+
     }
 }
 
+function reset() {
+    display.textContent = "";
+    firstNum = 0;
+    secondNum = 0;
+    operator = "";
+    operatorBtns.forEach(btn => btn.classList.remove("active"));
+}
+
+
 const display = document.querySelector(".display");
-const buttons = document.querySelector(".buttons");
 const digits = document.querySelectorAll(".digit");
 
-digits.forEach(digit => digit.addEventListener('click', () => display.textContent += digit.textContent)); 
+digits.forEach(digit => digit.addEventListener('click', () => {
+    if (resultOnDisplay) {
+        resultOnDisplay = false;
+        display.textContent = "";
+    }
+    display.textContent += digit.textContent;
+    operatorBtns.forEach(btn => btn.classList.remove("active"));
+})); 
 // Adds Event Listener for every Digit (0-9)
+
 
 const delBtn = document.querySelector(".delete");
 const clearBtn = document.querySelector(".clear");
 
 delBtn.addEventListener('click', () => display.textContent = display.textContent.slice(0, -1));
 
-clearBtn.addEventListener('click', () => {
-    display.textContent = ""
-    firstNum = 0;
-    secondNum = 0;
-    operator = "";
-});
+clearBtn.addEventListener('click', () => reset());
+
 
 const operatorBtns = document.querySelectorAll(".operator");
 const operationBtn = document.querySelector(".operation");
 
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+let resultOnDisplay = false;
 
 operatorBtns.forEach((btn) => btn.addEventListener('click', () => {
-        firstNum = display.textContent;
-        display.textContent = "";
-        operator = btn.textContent;
-        btn.classList.add("active");
+
+    operator = btn.textContent;
+    firstNum = display.textContent;
+    display.textContent = "";
+    btn.classList.add("active");
+
     }));
 // Adds Event Listener for every Operator (+, -, *, /)
-
 
 operationBtn.addEventListener('click', () => {
     secondNum = display.textContent;
     display.textContent = operate(operator, Number(firstNum), Number(secondNum));
-    operatorBtns.forEach(btn => btn.classList.remove("active"));
-})
-
+    resultOnDisplay = true;
+});
