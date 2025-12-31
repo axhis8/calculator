079ca9a3b88ac7d3d9f4d27f4a19311064f5403c
ashis <161737873+axhis8@users.noreply.github.com>
2025-12-31 11:27:55 +0100

    Update roundDecimal function
    Simplified the code and if the last digit of the decimal is 0, it'll remove it.

--- script.js
+++ script.js
@@ -39,13 +39,16 @@ function roundDecimal(num) {
     if (num.includes(".")) {
         let numArray = num.split(".");
 
-        if (numArray[1].length > 12) return Number(num).toFixed(2);
+        if (numArray[1].length > 12) {
+            num = Number(num).toFixed(2);
+            return num.endsWith("0") ? num.slice(0, -1) : num;
+        }
     }
-    
+        
     return num;
 }
 // Checks if the Result has Decimals, splits it into an Array and checks if the Length is above the Display Limit. 
-// If so, set the decimals only to two. 
+// If so, set the decimals only to two & if 0 is the last digit, remove it.
 // This also handles the problem with JavaScripts Rounding on decimal numbers.
 
 function reset() {
