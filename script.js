0ca02a6b3339fec227261665f5a0e96188dadcfc
ashis <161737873+axhis8@users.noreply.github.com>
2026-01-01 03:39:20 +0100

    Convert calculator to object style

--- script.js
+++ script.js
@@ -1,216 +1,224 @@
-const display = document.querySelector(".display");
-const digits = document.querySelectorAll(".digit");
+/* =========================
+    1. DOM
+========================= */
 
-const delBtn = document.querySelector(".delete");
-const clearBtn = document.querySelector(".clear");
-
-const operatorBtns = document.querySelectorAll(".operator");
-const operationBtn = document.querySelector(".operation");
-
-const decimalBtn = document.querySelector(".dot");
-
-const toggleSignBtn = document.querySelector(".plus-minus");
-const percentBtn = document.querySelector(".percent");
-
-let firstNum = 0;
-let secondNum = 0;
-let operator = "";
-
-let resultOnDisplay = false;
-let toggleOperators = false;
-let operatorBtnPressed = false;
-
-
-function add(a, b) {
-    return a + b;
+const display = {
+    div: document.querySelector(".display"),
+    getText() { return this.div.textContent; },
+    setText(text) { this.div.textContent = text; },
+    MAX_DISPLAY_DIGITS: 12
 }
 
-function subtract(a, b) {
-    return a - b;
+const button = {
+    digits: document.querySelectorAll(".digit"),
+    deleteDigit: document.querySelector(".delete"),
+    clear: document.querySelector(".clear"),
+    operators: document.querySelectorAll(".operator"),
+    operate: document.querySelector(".operation"),
+    decimal: document.querySelector(".dot"),
+    toggleSign: document.querySelector(".plus-minus"),
+    percent: document.querySelector(".percent")
 }
 
-function multiply(a, b) {
-    return a * b;   
+/* =========================
+    2. STATE
+========================= */
+
+const state = {
+    firstNum: null,
+    secondNum: null,
+    operator: "",
+    resultOnDisplay: false,
+    toggleOperators: false,
+    operatorBtnPressed: false
 }
 
-function divide(a, b) {
-   return b === 0 ? "ERROR" : a / b;
-}
-// Checks if a number is divided by 0, if so return "ERROR"
+/* =========================
+    3. MATH
+========================= */
 
-function operate(operator, a, b) {
+const math = {
+    add(a, b) { return a + b; },
+    subtract(a, b) { return a - b; },
+    multiply(a, b) { return a * b; },
+    divide(a, b) { return b === 0 ? "ERROR" : a / b; }, // Checks if a number is divided by 0, if so return "ERROR"   
 
-    switch (operator) {
-        
-        case "+": 
-            return add(a, b);
+    operate(operator, a, b) {
+        switch (operator) {
+            
+            case "+": 
+                return this.add(a, b);
 
-        case "-":
-            return subtract(a, b)
+            case "-":
+                return this.subtract(a, b)
 
-        case "×":
-            return multiply(a, b);
+            case "×":
+                return this.multiply(a, b);
 
-        case "÷":
-            return divide(a, b);
+            case "÷":
+                return this.divide(a, b);
 
-    }
-}
-
-function calculateResult() {
-    resultOnDisplay = true;
-    secondNum = display.textContent;
-
-    display.textContent = roundDecimal(operate(operator, Number(firstNum), Number(secondNum)));
-    // Equates the given digits & rounds the Decimals so it doesn't overpopulate the Display
-
-}
-
-function roundDecimal(num) {
-
-    num = String(num);
-    if (num.includes(".")) {
-        let numArray = num.split(".");
-
-        if (numArray[1].length > 12) {
-            num = Number(num).toFixed(2);
-            return num.endsWith("0") ? num.slice(0, -1) : num;
+            default:
+                return null;
         }
     }
-        
-    return num;
 }
+
+/* =========================
+    4. ACTIONS / INPUT
+========================= */
+
+const input = {
+    calculate() {
+        state.operatorBtnPressed = false;
+        state.resultOnDisplay = true;
+        state.secondNum = display.getText();
+        display.setText(this.roundDecimal(math.operate(state.operator, Number(state.firstNum), Number(state.secondNum))));
+        // Equates the given digits & rounds the Decimals so it doesn't overpopulate the Display
+    },
+
+    roundDecimal(num) {
+        num = String(num);
+        if (num.includes(".")) {
+            let numArray = num.split(".");
+
+            if (numArray[1].length > 12) {
+                num = Number(num).toFixed(2);
+                return num.endsWith("0") ? num.slice(0, -1) : num;
+            }
+        }
+        return num;
+    }, 
 // Checks if the Result has Decimals, splits it into an Array and checks if the Length is above the Display Limit. 
 // If so, set the decimals only to two & if 0 is the last digit, remove it.
 // This also handles the problem with JavaScripts Rounding on decimal numbers.
-
-function typeDigit(num) {
-
-    if (resultOnDisplay) {
-        resultOnDisplay = false;
-        display.textContent = "";
-    }
-
-    if (toggleOperators) {
-        toggleOperators = false;
-    }
-
-    display.textContent += num;
-    operatorBtns.forEach(btn => btn.classList.remove("active"));
-}
-
-function checkDecimal() {
-    if (display.textContent.includes(".")) return "";
-    else if (display.textContent === "") return "0.";
-    else return ".";
-} 
-// Checks if the Display already contains a decimal, if so don't return one
-
-function toggleClassActive(btn) {
-    btn.classList.add("active");
-    setTimeout(() => btn.classList.remove("active"), 100);
-}
-// Adds Animation for Buttons when clicked with a keyboard key
-
-function reset() {
-    display.textContent = "";
-    operator = "";
-    firstNum = 0;
-    secondNum = 0;
-    resultOnDisplay = false;
-    operatorBtnPressed = false;
     
-    operatorBtns.forEach(btn => btn.classList.remove("active"));
+    typeDigit(num) {
+
+        if (state.resultOnDisplay) {
+            state.resultOnDisplay = false;
+            display.setText("");
+        }
+        if (state.toggleOperators) {
+            state.toggleOperators = false;
+        }
+
+        display.setText(display.getText() + num);
+        button.operators.forEach(btn => btn.classList.remove("active"));
+    },
+
+    typeOperator(btn) {
+
+        if (state.toggleOperators) {
+            button.operators.forEach(btn => btn.classList.remove("active"));
+        } // Toggles Operator, without changing results
+
+        else if (state.operatorBtnPressed) {
+            input.calculate();
+            state.firstNum = display.getText();
+        } // Checks if a Operator is already pressed, so that it equates the numbers & shows it on the Display
+
+        else {
+            state.firstNum = display.getText();
+            state.operatorBtnPressed = true;
+            display.setText("");
+        }
+
+        state.operator = btn.textContent;
+        state.toggleOperators = true;
+        btn.classList.add("active");
+    },
+
+    checkDecimal() {
+        if (display.getText().includes(".")) return "";
+        else if (display.getText() === "") return "0.";
+        else return ".";
+    },
+    // Checks if the Display already contains a decimal, if so don't return one
+
+    toggleClassActive(btn) {
+        btn.classList.add("active");
+        setTimeout(() => btn.classList.remove("active"), 100);
+    },
+    // Adds Animation for Buttons when clicked with a keyboard key
+
+    deleteLastDigit() { display.setText(display.getText().slice(0, -1)); },
+
+    addDecimal() { display.setText(display.getText() + input.checkDecimal()); },
+
+    toPercent() { display.setText(display.getText() / 100); },
+
+    toggleSign() { display.setText(display.getText() * -1) },
+ 
+    reset() {
+        display.setText("");
+        state.operator = "";
+        state.firstNum = null;
+        state.secondNum = null;
+        state.resultOnDisplay = false;
+        state.operatorBtnPressed = false;
+        button.operators.forEach(btn => btn.classList.remove("active"));
+    }    
 }
 
-const getCurrentNum = () => Number(display.textContent);
+/* =========================
+    5. INIT
+========================= */
 
+function init() {
+    button.clear.addEventListener('click', input.reset);
+    button.percent.addEventListener('click', input.toPercent);
+    button.decimal.addEventListener('click', input.addDecimal);
+    button.toggleSign.addEventListener('click', input.toggleSign);
+    button.deleteDigit.addEventListener('click', input.deleteLastDigit);
 
-clearBtn.addEventListener('click', reset);
+    button.digits.forEach(digit => digit.addEventListener('click', () => input.typeDigit(digit.textContent)));
 
-decimalBtn.addEventListener('click', () => display.textContent += checkDecimal());
+    button.operators.forEach(btn => btn.addEventListener('click', () => input.typeOperator(btn))); 
+    // Adds Event Listener for every Operator (+, -, *, /)
 
-percentBtn.addEventListener('click', () => display.textContent = getCurrentNum() / 100);
-// Toggles the Number to percent
-
-toggleSignBtn.addEventListener('click', () => display.textContent = getCurrentNum() * -1);
-// Toggles sign of the displayed Number
-
-digits.forEach(digit => digit.addEventListener('click', () => typeDigit(digit.textContent)));
-// Adds Event Listener for every Digit (0-9)
-
-delBtn.addEventListener('click', () => display.textContent = display.textContent.slice(0, -1));
-
-
-operationBtn.addEventListener('click', () => {
-    if (!firstNum) return;
-
-    calculateResult();
-    operatorBtnPressed = false;
-});
-
-operatorBtns.forEach(btn => btn.addEventListener('click', (e) => {
-
-    if (toggleOperators) {
-        operatorBtns.forEach(btn => btn.classList.remove("active"));
-    }
-// Toggles Operator, without changing results
-
-    else if (operatorBtnPressed) {
-        calculateResult();
-        firstNum = display.textContent;
-    } 
-// Checks if a Operator is already pressed, so that it equates the numbers & shows it on the Display
-
-    else {
-        firstNum = display.textContent;
-        display.textContent = "";
-        operatorBtnPressed = true;
-    }
-
-    operator = btn.textContent;
-    btn.classList.add("active");
-    toggleOperators = true;
-
-    }));
-// Adds Event Listener for every Operator (+, -, *, /)
-
-
-document.addEventListener('keydown', (e) => {
-
-    // Digits
-    digits.forEach(digit => {
-        if (e.key === digit.textContent) {
-        typeDigit(digit.textContent);
-        toggleClassActive(digit);
-        }
+    button.operate.addEventListener('click', () => {
+        if (state.firstNum === null) return;
+        input.calculate();
     });
 
-    // Operators / Specials
-    switch (e.key) {
+    document.addEventListener('keydown', (e) => {
 
-        case "Backspace":
-            display.textContent = display.textContent.slice(0, -1);
-            toggleClassActive(delBtn);
-            break;
+        // Digits
+        button.digits.forEach(digit => {
+            if (e.key === digit.textContent) {
+                input.typeDigit(digit.textContent);
+                input.toggleClassActive(digit);
+            }
+        });
 
-        case "%":
-            display.textContent = getCurrentNum() / 100;
-            toggleClassActive(percentBtn);
-            break;
+        // Operators / Specials
+        switch (e.key) {
 
-        case ".":
-            display.textContent += checkDecimal();
-            toggleClassActive(decimalBtn);
-            break;
+            case "Backspace":
+                input.deleteLastDigit();
+                input.toggleClassActive(button.deleteDigit);
+                break;
 
-        case "Enter":
-            e.preventDefault();   
-            if (!firstNum) return;
+            case "%":
+                input.toPercent();
+                input.toggleClassActive(button.percent);
+                break;
 
-            toggleClassActive(operationBtn);
-            calculateResult();
-            operatorBtnPressed = false;            
-            break;
-    }
-});
+            case ".":
+                input.addDecimal();
+                input.toggleClassActive(button.decimal);
+                break;
+
+            case "Enter":
+                e.preventDefault();   
+                if (!state.firstNum) return;
+
+                input.toggleClassActive(button.operate);
+                input.calculate();
+                break;
+        }
+    });
+}
+
+init();
